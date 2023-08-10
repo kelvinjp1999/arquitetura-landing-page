3df8702115df7fe4f8dbe2e51c9e6c9df4a7c905
kelvin jean perosa <kelvinperosa.kp@gmail.com>
2023-08-10 12:42:14 -0300

    arquitetura-landing-page

---
+++ index.js
@@ -0,0 +1,14 @@
+var ambiente1=document.getElementById('ambiente1')
+var ambiente2=document.getElementById('ambiente2')
+
+function MudarAmbiente1(){
+    ambiente1.style='display:none'
+    ambiente2.style='display:flex'
+}
+
+function MudarAmbiente2(){
+    ambiente1.style='display:flex'
+    ambiente2.style='display:none'
+}
+
+
